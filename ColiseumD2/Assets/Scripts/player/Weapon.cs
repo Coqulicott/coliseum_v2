653387be1d587b012e0a56458c5f5e6b673db42f
Arthur <arthur.moine@free.fr>
2020-04-21 22:54:57 +0200

    yay

---
+++ ColiseumD2/Assets/Scripts/player/Weapon.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Coliseum
+{
+
+    public class Weapon : MonoBehaviour
+    {
+        public float attackDamage;
+        public float attackRange;
+        public float cooldown;
+        public string weapon;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            AssignStat();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void AssignStat()
+        {
+            switch (weapon)
+            {
+                case "marteau":
+                    attackDamage = 12;
+                    attackRange = 3;
+                    cooldown = 15;
+                    break;
+
+                case "lance":
+                    attackDamage = 6;
+                    attackRange = 6;
+                    cooldown = 5;
+                    break;
+
+                case "lame":
+                    attackDamage = 3;
+                    attackRange = 1;
+                    cooldown = 2;
+                    break;
+
+                case "claymore":
+                    attackDamage = 10;
+                    attackRange = 4;
+                    cooldown = 8;
+                    break;
+
+                case "":
+                    attackDamage = 1;
+                    attackRange = 1;
+                    cooldown = 1;
+                    break;
+            }
+        }
+    }
+}
